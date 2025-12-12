abc0e90ab8b1271ab0414495fd003461e54cd2ff
Wolfborn <josh.wolfborn@gmail.com>
2025-12-11 19:08:56 -0500

    Add Player

--- constants.py
+++ constants.py
@@ -1,2 +1,12 @@
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+# the radius of the player's ship
+PLAYER_RADIUS = 20 
+
+# the width of the lines that draw the player's ship
+LINE_WIDTH = 2
+
+PLAYER_TURN_SPEED = 300
+
+PLAYER_SPEED = 200
